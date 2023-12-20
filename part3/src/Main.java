d95aa20c6733121053b334f161ff6bf58548e90b
Armaritto <armia.joseph35@gmail.com>
2023-12-20 09:56:29 +0200

    part3 finished

--- part3/src/Main.java
+++ part3/src/Main.java
@@ -1,12 +1,13 @@
-import java.util.LinkedList;
-import java.util.Queue;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
         Tree tree = new Tree();
-        tree.Solution();
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 for iterative solution and 2 for recursive solution: ");
+        int x = sc.nextInt();
+        if(x == 1)
+            tree.SolutionIterative();
+        else if(x == 2)
+            tree.SolutionRecursive();
     }
 }
--- part3/src/Tree.java
+++ part3/src/Tree.java
@@ -1,15 +1,15 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Tree {
     private boolean initialize = true;
     private Queue<Node> nodes = new LinkedList<Node>();
-    public void Solution(){
+    public void SolutionRecursive(){
         int x;
         Node root = new Node();
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the value of the root node: ");
         x = sc.nextInt();
         if(x != -1) {
@@ -18,21 +18,45 @@ public class Tree {
         }else{
             return;
         }
-
         System.out.print("PreOrder Traversal: ");
-        preOrderTraversal(root);
+        preOrderTraversalRecursive(root);
         System.out.println();
 
         System.out.print("InOrder Traversal: ");
-        inOrderTraversal(root);
+        inOrderTraversalRecursive(root);
         System.out.println();
 
         System.out.print("PostOrder Traversal: ");
-        postOrderTraversal(root);
+        postOrderTraversalRecursive(root);
         System.out.println();
 
     }
+    public void SolutionIterative(){
+        int x;
+        Node root = new Node();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of the root node: ");
+        x = sc.nextInt();
+        if(x != -1) {
+            root.setValue(x);
+            root = handleInput(root);
+        }
+        else {
+            return;
+        }
 
+        System.out.print("PreOrder Traversal: ");
+        preOrderTraversalIterative(root);
+        System.out.println();
+
+        System.out.print("InOrder Traversal: ");
+        inOrderTraversalIterative(root);
+        System.out.println();
+
+        System.out.print("PostOrder Traversal: ");
+        postOrderTraversalIterative(root);
+        System.out.println();
+    }
     public Node handleInput(Node node){
 
         //
@@ -81,34 +105,88 @@ public class Tree {
         return rootNode;
     }
 
-    public void postOrderTraversal(Node node){
+    public void postOrderTraversalRecursive(Node node){
         if(node.getLeft() != null)
-            postOrderTraversal(node.getLeft());
+            postOrderTraversalRecursive(node.getLeft());
 
         if(node.getRight() != null)
-            postOrderTraversal(node.getRight());
+            postOrderTraversalRecursive(node.getRight());
 
         System.out.print(node.getValue() + " ");
     }
 
-    public void inOrderTraversal(Node node){
+    public void inOrderTraversalRecursive(Node node){
         if(node.getLeft() != null)
-            inOrderTraversal(node.getLeft());
+            inOrderTraversalRecursive(node.getLeft());
 
         System.out.print(node.getValue() + " ");
 
         if(node.getRight() != null)
-            inOrderTraversal(node.getRight());
+            inOrderTraversalRecursive(node.getRight());
     }
 
-    public void preOrderTraversal(Node node){
+    public void preOrderTraversalRecursive(Node node){
         System.out.print(node.getValue() + " ");
 
         if(node.getLeft() != null)
-            preOrderTraversal(node.getLeft());
+            preOrderTraversalRecursive(node.getLeft());
 
         if(node.getRight() != null)
-            preOrderTraversal(node.getRight());
+            preOrderTraversalRecursive(node.getRight());
+    }
+    public void postOrderTraversalIterative(Node root){
+        if (root == null) {
+            return;
+        }
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        stack1.push(root);
+        while(!stack1.isEmpty()) {
+            Node node = stack1.pop();
+            stack2.push(node);
+            if (node.getLeft() != null) {
+                stack1.push(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                stack1.push(node.getRight());
+            }
+        }
+        while(!stack2.isEmpty()){
+            System.out.print(stack2.pop().getValue() + " ");
+        }
+    }
+    public void inOrderTraversalIterative(Node root){
+        if (root == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        Node node = root;
+        while(!stack.isEmpty() || node != null){
+            while(node != null){
+                stack.push(node);
+                node = node.getLeft();
+            }
+            node = stack.pop();
+            System.out.print(node.getValue() + " ");
+            node = node.getRight();
+        }
+    }
+    public void preOrderTraversalIterative(Node root){
+        if (root == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            System.out.print(node.getValue() + " ");
+            if(node.getRight() != null){
+                stack.push(node.getRight());
+            }
+            if(node.getLeft() != null){
+                stack.push(node.getLeft());
+            }
+        }
     }
 }
 
