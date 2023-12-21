34fdce2893b72064a8028df5779a47cb5d39be42
Armaritto <armia.joseph35@gmail.com>
2023-12-21 19:39:04 +0200

    Final Part 1
    Co-Authored-By: Andrew Safwat <98947690+AndrewSafwat1@users.noreply.github.com>

--- part1/src/Graph.java
+++ part1/src/Graph.java
@@ -1,161 +1,76 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Graph {
-    // No. of vertices
-    private int v;
-    private ArrayList<Node>[] adj;
-    private ArrayList<Node> nodes = new ArrayList<>();
-    private int[][] cost;
-    private int minCost;
-    private ArrayList<Node> shortestPath = new ArrayList<>();
-    public ArrayList<Node> getShortestPath() {
-        return shortestPath;
+    private int[][] adjacencyMatrix;
+    private int[] parent;
+    public Graph(int vertices){
+        adjacencyMatrix = new int[vertices][vertices];
+        parent = new int[vertices];
     }
-    public void setShortestPath(ArrayList<Node> shortestPath) {
-        this.shortestPath = shortestPath;
+    public void addEdge(int source, int destination, int weight){
+        adjacencyMatrix[source][destination] = weight;
+        adjacencyMatrix[destination][source] = weight;
     }
-    public int getMinCost() {
-        return minCost;
+    public int[][] getAdjacencyMatrix() {
+        return adjacencyMatrix;
     }
-    public void setMinCost(int minCost) {
-        this.minCost = minCost;
-    }
-    public Graph(int v, char[] vertices) {
-        this.v = v;
-        adj = new ArrayList[v];
-        for (int i = 0; i < v; ++i){
-            adj[i] = new ArrayList<>();
-            nodes.add(new Node(vertices[i])); //also saved at place i
+    private String print2dArray(int[][] array) {
+        String result = "";
+        for(int i = 0; i < array.length; i++) {
+            result += Arrays.toString(array[i]) + "\n";
         }
-        cost = new int[v][v];
+        return result;
     }
-    void addEdge(char v, char w) {
-        //adding edge for both nodes as it is undirected
-        if(v == w){
-            adj[v-65].add(nodes.get(w-65));
-            return;
-        }
-        adj[v-65].add(nodes.get(w-65));
-        adj[w-65].add(nodes.get(v-65));
-    }
-    void addCost(char v, char w, int cost){
-        this.cost[v-65][w-65] = cost;
-        this.cost[w-65][v-65] = cost;
-    }
-    public ArrayList<Node> getAdj(char v){
-        return adj[v-65];
-    }
-    public int getCost(char v, char w){
-        return cost[v-65][w-65];
-    }
-//    public void dijkstra2(char source, char destination ) {
-//        //initializing the distance array
-//        int[] distance = new int[v];
-//        for (int i = 0; i < v; i++) {
-//            distance[i] = Integer.MAX_VALUE;
-//        }
-//        //initializing the visited array
-//        boolean[] visited = new boolean[v];
-//        //initializing the parent array
-//        int[] parent = new int[v];
-//        //initializing the priority queue
-//        //setting the source distance to 0
-//        distance[source - 65] = 0;
-//        //adding the source node to the priority queue
-//        pq.add(nodes.get(source - 65));
-//        //setting the parent of the source node to -1
-//        parent[source - 65] = -1;
-//        //while the priority queue is not empty
-//        while (!pq.isEmpty()) {
-//            //polling the top node
-//            Node node = pq.poll();
-//            //marking the node as visited
-//            visited[node.getValue() - 65] = true;
-//            //for each adjacent node
-//            for (Node n : adj[node.getValue() - 65]) {
-//                //if the node is not visited
-//                if (!visited[n.getValue() - 65]) {
-//                    //if the distance of the adjacent node is greater than the distance of the current node + the cost of the edge between the current node and the adjacent node
-//                    if (distance[n.getValue() - 65] > distance[node.getValue() - 65] + getCost(node.getValue(), n.getValue())) {
-//                        //updating the distance of the adjacent node
-//                        distance[n.getValue() - 65] = distance[node.getValue() - 65] + getCost(node.getValue(), n.getValue());
-//                        //adding the adjacent node to the priority queue
-//                        pq.add(n);
-//                        //setting the parent of the adjacent node to the current node
-//                        parent[n.getValue() - 65] = node.getValue();
-//                    }
-//                }
-//            }
-//        }
-//        //setting the minCost to the distance of the destination node
-//        minCost = distance[destination - 65];
-//        //adding the destination node to the shortest path
-//        shortestPath.add(nodes.get(destination - 65));
-//        //setting the current node to the destination node
-//        int curr = destination - 65;
-//        //while the parent of the current node is not -1
-//        while (parent[curr] != -1) {
-//            //adding the parent of the current node to the shortest path
-//            shortestPath.add(nodes.get(parent[curr]));
-//            //setting the current node to the parent of the current node
-//            curr = parent[curr];
-//            //??
-//
-//
-//        }
-//    }
-    public void dijkstra(char source ,char destination){
-        boolean flag = false;
-        HashMap<Node,Integer> distanceFromSource = new HashMap<>(); // cost to get to each node from the source
-        for(int i = 0; i < v; i++){
-            distanceFromSource.put(nodes.get(i),Integer.MAX_VALUE);
-        }
-        distanceFromSource.put(nodes.get(source-65),0);
-        Stack<Node> path = new Stack<>();
-        path.push(nodes.get(source-65));
-        int i = source-65;
-        while(!flag){
-            Node currerntNode = nodes.get(i);
-            ArrayList<Node> adjNodes = getAdj(currerntNode.getValue());
-            for(Node node : adjNodes) {
-                int cost = getCost(currerntNode.getValue(), node.getValue());
-                if (distanceFromSource.get(node) > distanceFromSource.get(currerntNode) + cost) {
-                    distanceFromSource.put(node, distanceFromSource.get(currerntNode) + cost);
+    public int[] dijkstra(int[][] graph, int source){
+        int v = graph.length;
+        int[] distance = new int[v];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[source] = 0;
+        parent[source] = -1;
+        boolean[] visited = new boolean[v];
+        for(int i=0;i<v-1;i++){
+            int current = -1;
+            for(int j=0;j<v;j++){
+                if(!visited[j] && (current == -1 || distance[j] < distance[current])){
+                    current = j;
                 }
             }
-            for(int j=0;j<distanceFromSource.size();j++) {
-                int minimum = Integer.MAX_VALUE;
-                if(distanceFromSource.get(nodes.get(j)) < minimum){
-                    minimum = distanceFromSource.get(nodes.get(j));
-                    i=j;
+            visited[current] = true;
+            for(int j=0;j<v;j++){
+                if(graph[current][j] != 0){
+                    int newDistance = distance[current] + graph[current][j];
+                    if(newDistance < distance[j]){
+                        distance[j] = newDistance;
+                        parent[j] = current;
+                    }
                 }
             }
-            path.push(nodes.get(i));
         }
+        return distance;
     }
-    public String getArrayOfCosts(){
-        String s = "";
-        for(int i = 0; i < cost.length; i++){
-            s += (char) (i+65);
-            s += ": [";
-            for(int j = 0; j < cost[i].length; j++){
-                if(cost[i][j] == 0)
-                    continue;
-                s += "{";
-                s += (char) (j+65) + " = ";
-                s += cost[i][j];
-                s += "}";
+    public ArrayList<Integer> getPath(int destination){
+        ArrayList<Integer> path = new ArrayList<>();
+        int current = destination;
+        while(current != -1){
+            for(int i=0;i<parent.length;i++){
+                if(i == current){
+                    path.add(i);
+                    current = parent[i];
+                }
             }
-            s += "] ";
         }
-        return s;
+        for(int i=0;i<path.size()/2;i++){
+            int temp = path.get(i);
+            path.set(i,path.get(path.size()-i-1));
+            path.set(path.size()-i-1,temp);
+        }
+        return path;
     }
     @Override
     public String toString() {
         return "Graph{" +
-                "v=" + v +
-                ", adj=" + Arrays.toString(adj) +
-                ", cost=" + getArrayOfCosts() +
+                "adjacencyMatrix=" + "\n" +
+                print2dArray(adjacencyMatrix) +
                 '}';
     }
-
 }
--- part1/src/Main.java
+++ part1/src/Main.java
@@ -1,18 +1,14 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter list of airports: ");
         char[] airports = scan.nextLine().replaceAll(", ","").toCharArray();
+        Graph g = new Graph(airports.length);
         System.out.print("Enter the flights: ");
         String[] flights = scan.nextLine().replaceAll(",","").split(" ");
-        Graph g = new Graph(airports.length, airports);
-        for(int i = 0; i < flights.length; i++) {
-            String[] temp = flights[i].split("-");
-            Character a = temp[0].charAt(0);
-            Character b = temp[1].charAt(0);
-            g.addEdge(a, b);
-        }
         System.out.println("The distance for each flight (in miles): ");
         for(int i = 0; i < flights.length; i++) {
             String[] temp = flights[i].split("-");
@@ -20,15 +16,19 @@ public class Main {
             Character b = temp[1].charAt(0);
             System.out.print(a + " to " + b + ": ");
             int cost = scan.nextInt();
-            g.addCost(a, b, cost);
+            g.addEdge((int)a - 65,(int)b - 65, cost);
         }
-        System.out.println("Enter source airport: ");
+        System.out.print("Enter source airport: ");
         char source = scan.next().charAt(0);
-        System.out.println("Enter destination airport: ");
+        System.out.print("Enter destination airport: ");
         char destination = scan.next().charAt(0);
-        g.dijkstra(source, destination);
-        System.out.println("Shortest path from " + source + " to " + destination + " is: ");
-        System.out.println(g.getShortestPath());
-        System.out.println("Total Distance: " + g.getMinCost());
+        int[] distance = g.dijkstra(g.getAdjacencyMatrix(), (int)source - 65);
+        ArrayList<Integer> path = g.getPath((int)destination - 65);
+        System.out.print("Shortest path from " + source + " to " + destination + " is: ");
+        for(int i = 0; i < path.size(); i++) {
+            System.out.print((char)(path.get(i)+65) + " ");
+        }
+        System.out.println();
+        System.out.println("Total distance: " + distance[(int)destination - 65] + " miles");
     }
 }
