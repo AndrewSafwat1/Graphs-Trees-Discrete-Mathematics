2f21b74445da2654322987d1cda3f56a28988311
AndrewSafwat1 <98947690+AndrewSafwat1@users.noreply.github.com>
2023-12-22 01:45:00 +0200

    Cleaned some code

--- part2/src/Graph.java
+++ part2/src/Graph.java
@@ -2,61 +2,60 @@ import java.util.*;
 
 public class Graph {
     //colors
-    private String[] colors = {"Red", "Green", "Blue", "Yellow", "Black", "White", "Grey", "Orange", "Rose", "Mint"};
+    private String[] colors = {"Red", "Blue", "Green", "Yellow", "Black", "White", "Grey", "Orange", "Rose", "Mint"};
 
-    // No. of vertices
-    private int v;
+    // No. of vertex
+    private int vertex;
 
     // Adjacency Lists
-    private ArrayList<Node>[] adj;
+    private ArrayList<Node>[] adjacencyList;
     private ArrayList<Node> nodes = new ArrayList<>();
 
-    // Constructor
-    public Graph(int v) {
-        this.v = v;
-        adj = new ArrayList[v];
-        for (int i = 0; i < v; ++i){
-            adj[i] = new ArrayList<>();
+    public Graph(int vertex) {
+        this.vertex = vertex;
+        adjacencyList = new ArrayList[vertex];
+        for (int i = 0; i < vertex; ++i){
+            adjacencyList[i] = new ArrayList<>();
             nodes.add(new Node(i)); //also saved at place i
         }
     }
 
-    // Function to add an edge into the graph
-    void addEdge(Integer v, Integer w) {
+    // add an edge into the graph
+    void addEdge(Integer vertex, Integer otherVertex) {
         //adding edge for both nodes as it is undirected
-        if(v == w){
-            adj[v].add(nodes.get(w));
+        if(vertex == otherVertex){
+            adjacencyList[vertex].add(nodes.get(otherVertex));
             return;
         }
-        adj[v].add(nodes.get(w));
-        adj[w].add(nodes.get(v));
+        adjacencyList[vertex].add(nodes.get(otherVertex));
+        adjacencyList[otherVertex].add(nodes.get(vertex));
     }
 
-    public ArrayList<Node> color() {
-        boolean flag =false;
+    public ArrayList<Node> colorGraph() {
+        boolean colorIsUsed =false;
         int maxEdges = 0;
         int vertexWithMaxEdges = 0;
 
 
         //Largest degree vertex is colored first
-        for (int i = 0; i < this.v; i++) {
-            if (this.adj[i].size() > maxEdges) {
-                maxEdges = this.adj[i].size();
+        for (int i = 0; i < this.vertex; i++) {
+            if (this.adjacencyList[i].size() > maxEdges) {
+                maxEdges = this.adjacencyList[i].size();
                 vertexWithMaxEdges = i;
             }
         }
         this.nodes.get(vertexWithMaxEdges).setColor(this.colors[0]);
 
-        for(int i = 0 ; i < this.v; i++){ //nodes in adj array
+        for(int i = 0 ; i < this.vertex; i++){ //nodes in adjacencyList array
             for(int j = 0 ; j < this.colors.length; j++){ //colors array
-                flag = false;
-                for(int k = 0 ; k < this.adj[i].size(); k ++){ //neighbours for every node
-                    if(this.colors[j].equals(this.adj[i].get(k).getColor())){
-                        flag = true;
+                colorIsUsed = false;
+                for(int k = 0 ; k < this.adjacencyList[i].size(); k ++){ //neighbours for every node
+                    if(this.colors[j].equals(this.adjacencyList[i].get(k).getColor())){
+                        colorIsUsed = true;
                         break;
                     }
                 }
-                if(flag)
+                if(colorIsUsed)
                     continue;
                 this.nodes.get(i).setColor(this.colors[j]);
                 break;
--- part3/src/Main.java
+++ part3/src/Main.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int x = 0;
         Tree tree = new Tree();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1 for iterative solution and 2 for recursive solution: ");
-        int x = sc.nextInt();
+        try {
+            x = sc.nextInt();
+        } catch (Exception e){
+            System.out.println("You didn't enter a value, please try again.");
+            System.exit(0);
+        }
+
         if(x == 1)
             tree.SolutionIterative();
         else if(x == 2)
